00d6be258187af9956cfeb7e47dc9da083f9bd4e
Alonzo Rico <juanlitthatbon@gmail.com>
2026-03-04 16:46:52 +0900

    Fixed pathing for the Home page

--- Check-Off/chirper/resources/views/home.blade.php
+++ Check-Off/chirper/resources/views/home.blade.php
@@ -5,22 +5,12 @@
     <title>check-off</title>
 </head>
 <body class="flex items-center">
-    @if (Route::has('login'))
     <header class="w-full flex items-center justify-end">
         <a href="" class="inline-block">home</a> 
         <a href="" class="inline-block">contributions</a>
-        @auth   
-            <a href="" class="inline-block"><img src="" alt=""></a>
-            <a href="" class="inline-block">event list</a>
-            <a href="" class="inline-block">logout</a>
-        @else
-            <a href="" class="inline-block">login</a>
-            @if (Route::has('signup'))
-                <a href="" class="inline-block">signup</a>
-            @endif
-        @endauth
+        <a href="" class="inline-block">login</a>
     </header>
-    @endif
+    
     <div class="flex items-center w-full justify-center">
         <h1>how do i use check-off?</h1>
         <p>for users: create an event, copy its link and share with your friends!<br/>for guests: visit shared link and enter your amount!</p>
