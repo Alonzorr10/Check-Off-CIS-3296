4d589668012591e608a72a60f64a9615495846c8
tunona <tuq29198@temple.edu>
2026-03-17 19:50:11 +0900

    split contributions pane

--- Check-Off/chirper/resources/views/contributions.blade.php
+++ Check-Off/chirper/resources/views/contributions.blade.php
@@ -1,27 +1,55 @@
 @extends('layouts.web')
 
 @section('content')
-
-    <div class="page" id="page-guest">
-    <div class="guest-box">
-      <div style="text-align:center;margin-bottom:1rem">
-        <div class="page-title">Join an Event</div>
-        <div class="page-sub">Enter your invite code to view and settle your contributions</div>
+<!--
+  <div class="page flex flex-col flex-1 items-center justify-around text-lg" id="page-guest">
+    <div class="guest-box flex flex-col flex-1 items-center justify-around">
+      <div class='flex flex-1 flex-col'>
+        <div class="flex-1 page-title">Join an Event</div>
+        <div class="flex-2 page-sub">Enter your invite code to view and settle your contributions</div>
       </div>
-      <div class="card" id="guestLookupCard">
-        <input class="guest-input" id="guestCode" placeholder="ABC123" maxlength="6" oninput="this.value=this.value.toUpperCase()">
-        <div style="margin-bottom:8px">
-          <div class="form-label" style="margin-bottom:4px">Your name</div>
-          <input class="form-input" id="guestName" placeholder="e.g. Sam">
+      <div class="card flex-1 flex flex-col" id="guestLookupCard">
+        <input class="guest-input flex-1" id="guestCode" placeholder="ABC123" maxlength="6" oninput="this.value=this.value.toUpperCase()">
+        <div class='flex flex-1'>
+          <div class="form-label flex-1" >Your name</div>
+          <input class="form-input flex-1" id="guestName" placeholder="e.g. Sam">
         </div>
-        <button class="btn btn-primary" style="width:100%;margin-top:8px" onclick="guestLookup()">View My Contributions</button>
+        <button class="flex btn btn-primary" onclick="guestLookup()">View My Contributions</button>
       </div>
-      <div id="guestResult"></div>
-      <div style="text-align:center;margin-top:1rem;font-size:13px;color:#888">
-        Have an account? <span style="color:#1D9E75;cursor:pointer">Log in</span>
+      <div class='flex flex-1' id="guestResult"></div>
+      <div class='flex flex-1'>
+        Have an account? <span >Log in</span>
       </div>
     </div>
   </div>
 </div>
+-->
+<form class='flex-1 flex flex-col items-center justify-evenly'>
+<!--
+  <div class='flex flex-1 flex-col gap-2'>
+    <label for='code' class='flex-1'>Code:</label>
+    <input name='code' class='flex-2 rounded-2xl bg-stone-100 border-0' placeholder='Enter code' oninput='this.value=this.value.toUpperCase()'/>
+  </div>
+-->
+  <div>
+    <input name='code1' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1' oninput='this.value=this.value.toUpperCase()'/>
+    <input name='code2' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1'oninput='this.value=this.value.toUpperCase()'/>
+    <input name='code3' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1'oninput='this.value=this.value.toUpperCase()'/>
+    <input name='code4' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1'oninput='this.value=this.value.toUpperCase()'/>
+    <input name='code5' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1'oninput='this.value=this.value.toUpperCase()'/>
+    <input name='code6' class='rounded bg-stone-100 border-0 w-[4ch] text-center' maxlength='1' minlength='1'oninput='this.value=this.value.toUpperCase()'/>
+  </div>
+  <input id='name' class='rounded-2xl bg-stone-100 border-0 text-center' placeholder='Enter your name' autocomplete='true'/>
+</form>
+<div class='flex-1 flex flex-col items-center'>
+  <div class='flex-1'>my contributions</div>
+  <div class='flex-1 flex-wrap'>
+    {{--@foreach($events as $e)--}}
+      <div class='flex-1 flex flex-col items-start justify-center bg-stone-400'>
+        <div class='flex-1 flex items-start justify-center'>temp{{--{{$e->name}}--}}</div>
+        <div class='flex-1 flex items-start justify-center'>$1k{{--{{$e->charge}}--}}</div>
+      </div>
+    {{--@endforeach--}}
+  </div>
+</div>
 @endsection
-    
